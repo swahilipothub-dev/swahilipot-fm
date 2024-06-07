9dbc5d6d52c966353bbab850a7781bcef07efaf2
kelvin <kitskelly38@gmail.com>
2024-06-07 10:42:29 +0300

    iniate all the necessary pages for the fm web app

---
+++ src/pages/news.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewsPage = () => {
+    return (
+        <div>
+            <h1>News Page</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default NewsPage;
