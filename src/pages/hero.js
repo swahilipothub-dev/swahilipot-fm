69259a1d0eb667d0b573746965a6432a64cdf133
Keshwrld <Vickykesh8@gmail.com>
2024-06-11 14:14:38 +0300

    Added news section under landing page

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -69,6 +69,34 @@ const Home = () => {
         </a>
         {/* End Card */}
       </div>
+      <div className="col-sm-6 col-lg-4 mb-3 mb-lg-5">
+        {/* Card */}
+        <a
+          className="card card-lg card-transition bg-primary-dark"
+          href="../blog-article.html"
+          style={{ minHeight: "22rem" }}
+        >
+          <div className="card-body">
+            <div className="mb-3">
+             <h3> <span className="badge bg-soft-light">Education</span> </h3>
+            </div>
+            <h4 className="card-title text-white mb-5">
+            New Curriculum Revolutionizes Learning
+            </h4>
+            <span className="card-link link-light">
+              Learn more <i className="bi-chevron-right small ms-1" />
+            </span>
+            <div className="position-absolute bottom-0 start-0 end-0">
+              <img
+                className="card-img"
+                src="/MACHOGU.jpg"
+                alt="Image Description"
+              />
+            </div>
+          </div>
+        </a>
+        {/* End Card */}
+      </div>
       {/* End Col */}
       <div className="col-sm-6 col-lg-4 mb-3 mb-lg-5">
         {/* Card */}
@@ -82,7 +110,7 @@ const Home = () => {
             <h3>  <span className="badge bg-soft-light">Music</span> </h3>
             </div>
             <h4 className="card-title text-white mb-5">
-              Space 2020 system architecture
+            The Evolution of Music
             </h4>
             <span className="card-link link-light">
               Learn more <i className="bi-chevron-right small ms-1" />
@@ -111,35 +139,7 @@ const Home = () => {
              <h3> <span className="badge bg-soft-light">Sports</span> </h3>
             </div>
             <h4 className="card-title text-white mb-5">
-              A guide to Space product materials
-            </h4>
-            <span className="card-link link-light">
-              Learn more <i className="bi-chevron-right small ms-1" />
-            </span>
-            <div className="position-absolute bottom-0 start-0 end-0">
-              <img
-                className="card-img"
-                src="/Mainoo.jpg"
-                alt="Image Description"
-              />
-            </div>
-          </div>
-        </a>
-        {/* End Card */}
-      </div>
-      <div className="col-sm-6 col-lg-4 mb-3 mb-lg-5">
-        {/* Card */}
-        <a
-          className="card card-lg card-transition bg-primary-dark"
-          href="../blog-article.html"
-          style={{ minHeight: "22rem" }}
-        >
-          <div className="card-body">
-            <div className="mb-3">
-             <h3> <span className="badge bg-soft-light">Education</span> </h3>
-            </div>
-            <h4 className="card-title text-white mb-5">
-              A guide to Space product materials
+            Team Clinches Championship Title
             </h4>
             <span className="card-link link-light">
               Learn more <i className="bi-chevron-right small ms-1" />
@@ -167,7 +167,7 @@ const Home = () => {
              <h3> <span className="badge bg-soft-light">Youth</span> </h3>
             </div>
             <h4 className="card-title text-white mb-5">
-              A guide to Space product materials
+            Young Innovators Lead the Way in Tech
             </h4>
             <span className="card-link link-light">
               Learn more <i className="bi-chevron-right small ms-1" />
@@ -175,7 +175,7 @@ const Home = () => {
             <div className="position-absolute bottom-0 start-0 end-0">
               <img
                 className="card-img"
-                src="/Mainoo.jpg"
+                src="/African-Youth.jpg"
                 alt="Image Description"
               />
             </div>
@@ -194,8 +194,8 @@ const Home = () => {
             <div className="mb-3">
              <h3> <span className="badge bg-soft-light">Environment</span> </h3>
             </div>
-            <h4 className="card-title text-white mb-5">
-              A guide to Space product materials
+            <h4 className="card-title text-white mb-5 position-bottom">
+            Sustainable Materials: A Guide to Eco-Friendly Choices
             </h4>
             <span className="card-link link-light">
               Learn more <i className="bi-chevron-right small ms-1" />
@@ -203,7 +203,7 @@ const Home = () => {
             <div className="position-absolute bottom-0 start-0 end-0">
               <img
                 className="card-img"
-                src="/Mainoo.jpg"
+                src="/ENV.jpg"
                 alt="Image Description"
               />
             </div>
@@ -217,8 +217,8 @@ const Home = () => {
     {/* Info */}
     <div className="text-center">
       <p className="mb-0">Want to read more?</p>
-      <a className="link" href="../blog-classic.html">
-        Explore all Space resources{" "}
+      <a className="link" href="/news">
+        Explore all news here{" "}
         <i className="bi-chevron-right small ms-1" />
       </a>
     </div>
