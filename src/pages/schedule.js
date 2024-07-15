fac5c409480b56a28af5def1040a89a8c02ab242
amiandah <amianda@swahilipothub.co.ke>
2024-07-15 11:44:04 +0300

    schedule page

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,9 +1,8 @@
 import Header from "@/components/Header";
 import Footer from "../components/Footer";
-import ContactSection from './Contactsection';
+import ContactSection from "../pages/Contactsection";
 import React, { useState } from 'react';
 import styles from '../styles/Schedule.module.css';
-import FrequencyDetails from '../components/FrequencyDetails';
 import { Table, Button } from 'react-bootstrap';
 
 function Schedule() {
@@ -25,32 +24,32 @@ function Schedule() {
 
     const scheduleData = {
         'Sunday': [
-            { time: '08:00 - 12:00', title: 'Morning Gospel', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
-            { time: '12:00 - 16:00', title: 'Sunday Afternoon Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
+            { time: '08:00 - 12:00', title: 'Morning Gospel', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
+            { time: '12:00 - 16:00', title: 'Sunday Afternoon Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
         ],
         'Monday': [
-            { time: '07:00 - 11:00', title: 'The Breakfast Vibe', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
-            { time: '11:00 - 16:00', title: 'Fresh Music Blue Vibe', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
+            { time: '07:00 - 11:00', title: 'The Breakfast Vibe', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
+            { time: '11:00 - 16:00', title: 'Fresh Music Blue Vibe', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
         ],
         'Tuesday': [
-            { time: '09:00 - 12:00', title: 'Tue-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
-            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
+            { time: '09:00 - 12:00', title: 'Tue-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
+            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
         ],
         'Wednesday': [
-            { time: '09:00 - 12:00', title: 'Wed-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
-            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
+            { time: '09:00 - 12:00', title: 'Wed-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
+            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
         ],
         'Thursday': [
-            { time: '09:00 - 12:00', title: 'Thur-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
-            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
+            { time: '09:00 - 12:00', title: 'Thur-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
+            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
         ],
         'Friday': [
-            { time: '09:00 - 12:00', title: 'Fri-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
-            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
+            { time: '09:00 - 12:00', title: 'Fri-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
+            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
         ],
         'Saturday': [
-            { time: '09:00 - 12:00', title: 'Sat-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
-            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: 'user.jpg' },
+            { time: '09:00 - 12:00', title: 'Sat-Morning Show', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
+            { time: '12:00 - 15:00', title: 'Lunchtime Mix', voiceover: 'Voiceover by: SWAHILIPOTFM', presenterImage: '/user.jpg' },
         ],
     };
 
@@ -58,16 +57,14 @@ function Schedule() {
         <section className="content-space-t-3">
             <Header />
             <main className="content">
-                <div><FrequencyDetails /></div>
                 <div className={styles.container}>
-                    <h1 className={styles.schedulecontent + ' bg-info'} style={{ textAlign: 'center' }}> Swahilipot FM Schedule
-                    </h1>
+                    <h1 className={styles.schedulecontent + ' bg-info'} style={{ textAlign: 'center' }}>Swahilipot FM Schedule</h1>
                     <div className={`${styles.scrollableTabs}`}>
                         <div className={styles.tabs}>
                             {tabs.map((tab) => (
                                 <Button
                                     key={tab}
-                                    variant={selectedTab === tab ? 'primary' : 'bg-success'}
+                                    variant={selectedTab === tab ? 'primary' : 'secondary'}
                                     onClick={() => handleTabClick(tab)}
                                     className={selectedTab === tab ? styles.active : ''}
                                 >
