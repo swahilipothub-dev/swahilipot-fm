a2e1d3de9bb657e58f69a44afc568120161d9853
mm-codes <c.mwalimo@swahilipothub.co.ke>
2024-08-13 07:11:32 +0300

    fix: embed responsiveness

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,7 +1,8 @@
 const VideoPlayer = ({ src }) => {
   return (
-    <div className='container'>
-      <iframe
+    <div className="container my-3">
+      <div className='ratio ratio-16x9'>
+        <iframe
         width='1000'
         height='500'
         src='https://www.youtube.com/embed/T_cM2XZqe18?si=-9YfScuM_uioqW4a'
@@ -10,7 +11,8 @@ const VideoPlayer = ({ src }) => {
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
         referrerpolicy='strict-origin-when-cross-origin'
         allowfullscreen
-      ></iframe>
+        ></iframe>
+      </div>
     </div>
   )
 }
